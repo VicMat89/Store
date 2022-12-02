554079053493728d078575fea199875ee3ef639d
PauloBazan <paulobazan155@gmail.com>
2022-12-02 21:22:55 +0100

    nuevos cambios

--- store/src/App.js
+++ store/src/App.js
@@ -1,7 +1,23 @@
-
+import axios from "axios";
+import { useState, useEffect } from "react";
+import React, { Component } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 function App() {
-  
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const info = await axios.get("https://fakestoreapi.com/products");
+      setProduct(info.data);
+    };
+    getData();
+    console.log(product);
+  }, []);
+
+  return <>
+  </>
 }
 
 export default App;
