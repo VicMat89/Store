43b1e53b41be665f3ceb4a29401e787661e644d1
PauloBazan <paulobazan155@gmail.com>
2022-12-07 19:41:29 +0100

    grid productos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import GridProducts from "./components/GridProducts";
 
 
 function App() {
-  
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+      const getData = async () => {
+          const info = await axios.get("https://fakestoreapi.com/products");
+          setProducts(info.data);
+
+      };
+      getData();
+  }, []);
+
+  return (
+    <>
+      <GridProducts products={products} />
+    </>
+  );
 }
 
 export default App;
