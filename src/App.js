71f9422be64d4ac0106457fd3f88cb66b0e57748
VicMat89 <vicgar89@gmail.com>
2022-12-09 20:07:16 +0100

    Carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import GridProducts from "./components/GridProducts";
+import './assets/App.css';
+import DemoCarousel from "./components/Carousel";
 
 
 function App() {
   const [products, setProducts] = useState([]);
+
 
   useEffect(() => {
       const getData = async () => {
@@ -16,10 +19,24 @@ function App() {
   }, []);
 
   return (
-    <>
+    <> 
+    <DemoCarousel info={products} />
       <GridProducts products={products} />
+
     </>
   );
 }
-
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
