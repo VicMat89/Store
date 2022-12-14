4160b0ece6554b2094af48670ce41f186b7b40f8
Rotosama <n.ruizgarcia@hotmail.com>
2022-12-14 21:22:03 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,45 +1,28 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import GridProducts from "./components/GridProducts";
-import './assets/App.css';
+import "./assets/App.css";
 import DemoCarousel from "./components/Carousel";
-import {MenuNav} from './components/MenuNav';
-
+import { MenuNav } from "./components/MenuNav";
 
 function App() {
   const [products, setProducts] = useState([]);
 
-
   useEffect(() => {
-      const getData = async () => {
-          const info = await axios.get("https://fakestoreapi.com/products");
-          setProducts(info.data);
-          console.log(info)
-
-      };
-      getData();
+    const getData = async () => {
+      const info = await axios.get("https://fakestoreapi.com/products");
+      setProducts(info.data);
+      console.log(info);
+    };
+    getData();
   }, []);
 
   return (
-    <> 
-    <DemoCarousel info={products} />
-      <MenuNav productos = {products}/>
+    <>
+      <MenuNav />
+      <DemoCarousel info={products} />
       <GridProducts products={products} />
-
     </>
   );
 }
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/components/MenuNav.jsx
+++ src/components/MenuNav.jsx
@@ -3,12 +3,14 @@ import '../assets/navstyles.css'
  
  export function MenuNav(props){
 
+    
+
     return(
         <div className='nav-container'>
             <div className="logo"><img src='https://img.freepik.com/vector-gratis/carro-tienda-edificio-tienda-dibujos-animados_138676-2085.jpg?w=2000' alt='logo'/></div>
             <div className="no-carr">Products</div>
             <div className="bestRated">Best Rated</div>
-            <input className='search'placeholder='Search...' type='text'/>
+            <input className='search' placeholder='Search...' type='text'/>
             <div className="user">User</div>
         </div>
 
